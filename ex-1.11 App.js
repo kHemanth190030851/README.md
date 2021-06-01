ae27f0279cb28ae26742cd9272b72c40edcf0898
kHemanth190030851 <74002361+kHemanth190030851@users.noreply.github.com>
2021-06-01 20:09:46 +0530

    Update ex-1.11 App.js

--- ex-1.11 App.js
+++ ex-1.11 App.js
@@ -1,23 +1,59 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-
-  return (
+const Header = (props) => {
+  return(
     <div>
-      {anecdotes[selected]}<br></br>
-      <button>next anecdotes</button>
+      <p>
+         {props.name}
+      </p>
     </div>
   )
 }
+
+const Content = (props) => {
+  return(
+    <div>
+      <p>
+        Content {props.part1} {props.part2} {props.part3} {props.exercises1} {props.exercises2} {props.exercises3}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <div>
+      <p>
+        Total Number of exercises 31
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  
+  return (
+    <div>
+      <Header name= {course}/>
+      <Content part1={part1}
+       Content exercises1 = {exercises1} />
+       <Content part2={part2}
+       Content exercises2 = {exercises2} />
+       <Content part3={part3}
+       Content exercises3 = {exercises3} />
+      <Total parts={exercises1+exercises2+exercises3} />
+    </div>
+  );
+};
+
+
 
 export default App
